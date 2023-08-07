c4348a1427220dfc007ba8dca43c33d6082ca742
苦麒麟 <kuqilin_1@163.com>
2023-08-07 21:21:55 +0800

    update MayDOS to 0.5.1

--- MayDOS_OOBE.py
+++ MayDOS_OOBE.py
@@ -1,9 +1,12 @@
 import os
 import time
 import base64
+import json
+import tkinter as tk
 from time import sleep
-from colorama import Fore, init
 
+if os.name == "nt":
+    os.system("")
 if os.path.isdir('MayDOS_Login/') == False:
     os.makedirs('MayDOS_Login/')
 if os.path.isdir('important/') == False:
@@ -16,20 +19,58 @@ if os.path.isdir('important/download') == False:
     os.makedirs('important/download')
 if os.path.isfile('important/Version.ver') == False:
     with open('important/Version.ver','w',encoding='gbk') as f:
-        f.write("V0.4.2 内测版\n")
+        f.write("V0.5.1 内测版\n")
         f.close()
 if os.path.isfile('important/download/cg.txt') == False:
     path_url = os.getcwd() + "\\"
     with open('important/download/cg.txt','w') as f:
         f.write(path_url)
         f.close()
+if os.path.isfile('important/Applications/sys.json') == False:
+    SYS_0 = {"Name":"0.5.1","description":"a cui dos"}
+    with open('important/Applications/sys.json','w') as f:
+        SYS_1 = json.dumps(SYS_0,sort_keys=True, indent=4, separators=(',', ':'))
+        f.write(SYS_1)
+        f.close()
 
-init(autoreset=True)
+# 彩色自定义文本
+class Style:
+    END = '\33[0m'
+    BOLD = '\33[1m'
+    ITALIC = '\33[3m'
+    URL = '\33[4m'
+    BLINK = '\33[5m'
+    BLINK2 = '\33[6m'
+    SELECTED = '\33[7m'
+
+class Font:
+    BLACK = '\33[30m'
+    RED = '\33[31m'
+    GREEN = '\33[32m'
+    YELLOW = '\33[33m'
+    BLUE = '\33[34m'
+    VIOLET = '\33[35m'
+    BEIGE = '\33[36m'
+    WHITE = '\33[37m'
+
+class Background:
+    BLACK = '\33[40m'
+    RED = '\33[41m'
+    GREEN = '\33[42m'
+    YELLOW = '\33[43m'
+    BLUE = '\33[44m'
+    VIOLET = '\33[45m'
+    BEIGE = '\33[46m'
+    WHITE = '\33[47m'
+
+tk = tk.Tk()
+
+tk.withdraw()
 
 check_files = os.listdir('important/')
 account_existed = check_files[0]
 if account_existed == 'account.user':
-    print(Fore.YELLOW + 'You have already registered')
+    print(f'{Font.YELLOW}You have already registered{Font.WHITE}')
     input('Press Enter to close...')
     os.system("START start.bat")
     quit()
@@ -40,7 +81,7 @@ try:
     version = readversion.read()
     readversion.close()
 except FileNotFoundError:
-    print(Fore.RED + 'ERROR_Version_File_Not_Found')
+    print(f'{Font.RED}ERROR_Version_File_Not_Found{Font.WHITE}')
     input('Press Enter to close...')
     quit()
 
@@ -200,3 +241,5 @@ while True:
         cls()
         print('请输入正确的选项！')
         pass
+
+tk.mainloop()
--- MayDOS_System.py
+++ MayDOS_System.py
@@ -1,22 +1,42 @@
-#colorama使用说明：
-#Fore.xxx  前景色（字体颜色）
-#Back.xxx 背景色
-#Fore常用颜色：
-#Fore.GREEN
-#Fore.CYAN
-#Fore.BLUE
-#Fore.RED
-#Fore.MAGENTA
-#Fore.BLACK
-#Fore.YELLOW
-#详情请在Py控制台导入colorama并输入help(Fore)
-#系统颜色表示说明：
-#绿色：提示，正确
-#红色：警告，错误
-#黄色：警告，错误（比红色弱一点）
-#浅蓝色：信息，属性
-#紫红色：暂定
-#黑色：无（黑色哪看得见？）
+"""
+colorama现已弃用
+
+字体样式 -->
+Font.BLACK     : 黑色
+Font.RED       : 红色
+Font.GREEN     : 绿色
+Font.YELLOW    : 黄色
+Font.BLUE      : 蓝色
+Font.VIOLET    :
+Font.BEIGE     : 青蓝色
+Font.WHITE     : 白色
+
+字体背景样式 -->
+Font.BLACK     : 黑色
+Font.RED       : 红色
+Font.GREEN     : 绿色
+Font.YELLOW    : 黄色
+Font.BLUE      : 蓝色
+Font.VIOLET
+Font.WHITE     : 绿色
+
+字体特性/特效 -->
+Font.END       : 终端默认设置
+Font.BOLD      : 高亮显示
+Font.ITALIC    : 斜体文本
+Font.URL       : 下划线
+Font.BLINK     : 闪烁
+Font.BLINK2    : 未知
+Font.SELECTED  : 反显
+
+系统颜色表示说明 -->
+绿色：提示，正确
+红色：警告，错误
+黄色：警告，错误（比红色弱一点）
+浅蓝色/青蓝色/蓝色：信息，属性
+紫红色：暂定
+黑色：无（黑色哪看得见？）
+"""
 
 import random
 import wget
@@ -25,15 +45,19 @@ import requests
 import os,sys
 import time
 import base64
-from time import sleep
-from colorama import Fore, Back, init
-import tkinter as tk
-import tkinter.ttk as ttk
 import tkinter.messagebox
+import asyncio
+from time import sleep
 
-os.system(r'title MayDOS')
-#os.system(r"mode con cols=1280 lines=648")
+os.system(r'title MayDOS') #更改标题
 
+# 环境设置
+if os.name == "nt":
+    os.system("")
+    
+"""
+自动生成/补全 部分文件
+"""
 if os.path.isdir('MayDOS_Login/') == False:
     os.makedirs('MayDOS_Login/')
 if os.path.isdir('important/') == False:
@@ -44,47 +68,128 @@ if os.path.isdir('important/log') == False:
     os.makedirs('important/log')
 if os.path.isdir('important/download') == False:
     os.makedirs('important/download')
+if os.path.isfile('important/download/per.txt') == False:
+    with open('important/download/per.txt','w') as f:
+        f.write('root')
+        f.close()
 
-root = tk.Tk()
+# 彩色自定义文本
+class Style:
+    END = '\33[0m'
+    BOLD = '\33[1m'
+    ITALIC = '\33[3m'
+    URL = '\33[4m'
+    BLINK = '\33[5m'
+    BLINK2 = '\33[6m'
+    SELECTED = '\33[7m'
 
-def show():
-    for i in range(100):
-        # 每次更新加1
-        progressbarOne['value'] = i + 1
-        # 更新画面
-        root.update()
-        time.sleep(0.01)
+class Font:
+    BLACK = '\33[30m'
+    RED = '\33[31m'
+    GREEN = '\33[32m'
+    YELLOW = '\33[33m'
+    BLUE = '\33[34m'
+    VIOLET = '\33[35m'
+    BEIGE = '\33[36m'
+    WHITE = '\33[37m'
+
+class Background:
+    BLACK = '\33[40m'
+    RED = '\33[41m'
+    GREEN = '\33[42m'
+    YELLOW = '\33[43m'
+    BLUE = '\33[44m'
+    VIOLET = '\33[45m'
+    BEIGE = '\33[46m'
+    WHITE = '\33[47m'
+
+# 系统权限API
+class SysPerAPI():
+    def __init__(self,Per : str = 'sys') -> None:
+        self.Per = Per
+        return None
+
+    def cls(self) -> int:
+        try:
+            os.system('cls')
+            return 200
+        except Exception as e:
+            print(f'{Font.RED}MayDOS/ROOT/ERROR>>>{e}{Font.WHITE}')
+            return 400
+
+    def CreatSysFile(self,path : str = '',context : str = 'Test') -> int:
+        try:
+            if self.Per == 'sys':
+                with open(path,'w',encoding='utf-8') as f:
+                    f.write(f'SYS_FILE\n{context}')
+                    f.close()
+                return 200
+            elif self.Per != 'sys':
+                print(f'{Font.YELLOW}Insufficient permissions{Font.WHITE}')
+                return 201
+            else:
+                print(f'{Font.YELLOW}Unknown permissions: {self.Per}{Font.WHITE}')
+                return 202
+        except Exception as e:
+            print(f'{Font.RED}MayDOS/Root/ERROR>>>{e}{Font.WHITE}')
+            return 400
+
+    def JsonFileRead(self,path):
+        try:
+            with open(path,'r') as f:
+                __date = f.read()
+                f.close()
+                return __date
+        except Exception as e:
+            print(f'{Font.RED}MayDOS/Root/ERROR>>>{e}')
+
+# 创建更新检测函数
+async def check_update_bar():
+    task = asyncio.create_task(check_update())
+    for i in range(1, 101):
+        print("\r", end="")
+        print("检测更新中: {}%: ".format(i), "▋" * (i // 2), end="")
+        sys.stdout.flush()
+    await task    
+    
+async def check_update():
+    # 读取更新日志
     Update = json.loads(requests.get("https://buelie.github.io/MayDOS/config.json").text)
-    code = "0.4.0"
-    if Update["latest"]["default"] != code:
+
+    # 当前版本号
+    CODE = "0.5.0"
+
+    # 比较版本号
+    if Update["latest"]["default"] != CODE:
         Y_N_U = tkinter.messagebox.askyesno(title='更新提示',message=f'有可用更新，是否下载?\n当前版本:{code} -> {Update["latest"]["default"]}\n稍等一下，马上就好，在important/download/找到更新程序并运行即可')
         if Y_N_U == True:
-            if os.path.isfile('important/download/main.py'):
-                os.remove("important/download/main.py")
+            # 如果条件为真则执行该线程
+
+            # 检测文件是否存在,如果存在则执行销毁操作
+            if os.path.isfile('update.py'):
+                os.remove("update.py")
             else:
                 pass
+
+            # 下载更新文件并运行
             wget.download("https://buelie.github.io/MayDOS/Update/update.py","")
             os.system("START update.py")
             print("\n")
+            
+            # 退出线程
             quit()
         else:
-            root.withdraw()
-        root.withdraw()
+            os.system("cls")
     else:
-        root.withdraw()
+        os.system("cls")
+# 变量设置
 
-Label = ttk.Label(root,text="检查更新中").pack()
-progressbarOne = tkinter.ttk.Progressbar(root)
-progressbarOne.pack(pady=20)
-progressbarOne['maximum'] = 100
-progressbarOne['value'] = 0
-progressbarOne['length'] = 200
-show()
+# 以asyncio调用check_update_bar函数
+asyncio.run(check_update_bar())
 
-init(autoreset=True)
+time_0 = 0.03 # 动画间隔时间
 
-time_0 = 0.05
-
+# 打印动画(不要将其存储进文本文件,会读取错误)
 print("MMMMMMMM               MMMMMMMM                                             DDDDDDDDDDDDD             OOOOOOOOO        SSSSSSSSSSSSSSS ")
 time.sleep(time_0)
 print("M:::::::M             M:::::::M                                             D::::::::::::DDD        OO:::::::::OO    SS:::::::::::::::S")
@@ -127,11 +232,9 @@ print("                                                 y:::::y                 
 time.sleep(time_0)
 print("                                                yyyyyyy                                                                                ")
 time.sleep(time_0)
-print("================================================================ May DOS V0.4.0 ================================================================")
+print("================================================================ May DOS V0.5.1 ================================================================")
 
-def cls():
-    os.system('cls')
-
+# 变量设置
 error_version_file_not_found = False
 error_account_file_not_found = False
 
@@ -158,8 +261,8 @@ try:
         username = str(base64.b64decode(username),'utf-8')
         password = str(base64.b64decode(password),'utf-8')
     except Exception as e:
-        print(Fore.RED + 'ERROR: 账户信息加载失败，账户文件可能损坏，请尝试注销并重新注册')
-        print(Fore.RED + 'ERROR_INFOMATION')
+        print(f'{Font.RED}ERROR: 账户信息加载失败，账户文件可能损坏，请尝试注销并重新注册{Font.WHITE}')
+        print(f'{Font.RED}ERROR_INFOMATION{Font.WHITE}')
         print(e)
         sleep(10)
         quit()
@@ -168,29 +271,29 @@ except FileNotFoundError:
     error_account_file_not_found = True
 
 if error_account_file_not_found == True and error_version_file_not_found == False:
-    print(Fore.RED + '未注册或账户文件异常丢失,可尝试启动OOBE修复')
+    print(f'{Font.RED}未注册或账户文件异常丢失,可尝试启动OOBE修复{Font.WHITE}')
     input('按下回车键退出...')
     quit()
 elif error_version_file_not_found == True and error_account_file_not_found == False:
-    print(Fore.RED + '系统版本文件被移动或异常丢失，请尝试联系我们以修复,可尝试启动OOBE修复')
+    print(f'{Font.RED}系统版本文件被移动或异常丢失，请尝试联系我们以修复,可尝试启动OOBE修复{Font.WHITE}')
     input('按下回车键退出...')
     quit()
 elif error_account_file_not_found  and error_version_file_not_found == True:
-    print(Fore.RED + '未找到系统版本文件及账户文件,可尝试启动OOBE修复')
-    print(Fore.RED + '请确认是否注册并尝试联系我们,可尝试启动OOBE修复')
+    print(f'{Font.RED}未找到系统版本文件及账户文件,可尝试启动OOBE修复{Font.WHITE}')
+    print(f'{Font.RED}请确认是否注册并尝试联系我们,可尝试启动OOBE修复{Font.WHITE}')
     input('按下回车键退出...')
     quit()
     
 if username == 'TEST':
     print(account_info)
 
-print(Fore.GREEN + 'Welcome!')
+print(f'{Font.GREEN}Welcome!')
 
 while True:
     if username == 'TEST':
-        print(Fore.BLUE + 'test_account_auto_login')
+        print(f'{Font.BLUE}test_account_auto_login{Font.WHITE}')
     else:
-        print(Fore.CYAN + f'登录{username}的电脑')
+        print(f'{Font.BEIGE}登录{username}的电脑{Font.WHITE}')
     
     if username == 'TEST':
         break
@@ -198,59 +301,163 @@ while True:
         userspassword = input('密码>')
 
     if userspassword == password:
-        print(Fore.GREEN + '密码正确')
+        print(f'{Font.GREEN}密码正确')
         break
     else:
-        print(Fore.RED + '密码错误！')
+        print(f'{Font.RED}密码错误！')
         pass
 
-sleep(1)
-cls()
+time.sleep(0.25)
+SysPerAPI().cls()
 
-print(Fore.GREEN + '正在准备你的MayDOS命令行......')
-print(Fore.GREEN + '请输入"usebook"以打开MayDOS0.1的使用手册和帮助')
-time.sleep(1)
+print(f'{Font.GREEN}正在准备你的MayDOS命令行......{Font.WHITE}')
+print(f'{Font.GREEN}请输入"usebook"以打开MayDOS0.1的使用手册和帮助{Font.WHITE}')
+time.sleep(0.02)
 
 while True:
     cmd = input('MayDOS/Root>>>')
     
-    if cmd == 'calc':
+    if cmd.lower() == 'calc' :
         os.system('python important/Applications/calc.py')
 
-    elif cmd[0:3] == 'sof':
-        os.system(f'python important/Applications/{cmd[4:-1]}')
+    elif cmd[0:3].lower() == 'sof':
+        try:
+            if cmd[4:-1] == 'json':
+                print(f"{Font.RED}MayDOS/Root>>>该文件您无权访问")
+            elif cmd[4:-1] == 'api' or cmd[4:-1] == 'api.py':
+                print(f"{Font.RED}MayDOS/Root>>>该文件您无权访问")
+            else:
+                if os.path.isfile(f'important/Applications/{cmd[4:-1]}/{cmd[4:-1]}.json') == False or os.path.isfile(f'important/Applications/{cmd[4:-1]}/{cmd[4:-1]}/MAIN.txt') == False:
+                    print(f'{Font.YELLOW}MayDos/Root/SOF>>>应用程序"{cmd[4:-1]}"可能已被恶意篡改,请谨慎运行。（Y/N)')
+                    Warn_0 = input()
+                    if Warn_0 == 'Y' or Warn_0 == 'y' or Warn_0 == 'YES' or Warn_0 == 'yes':
+                        try:
+                            os.system(f'START /MAX "important/Applications/{cmd[4:-1]}/{cmd[4:-1]}.bat"')
+                        except Exception as e:
+                            print(f'{Font.RED}MayDOS/Root>>>{e}')
+                        except FileNotFoundError as e:
+                            print(f'{Font.RED}MayDOS/Root>>>{e}')
+                    elif Warn_0 == 'N' or Warn_0 == 'n' or Warn_0 == 'NOT' or Warn_0 == 'not':
+                        pass
+                    else:
+                        print("未知操作，已自动退出...")
+        except Exception as e:
+            print(f'{Font.RED}MayDOS/Root>>>{e}')
+
+    elif cmd[0:4].lower() = 'down':
+        print("=======================================")
+        print("|请选择下载源                         |            \n|1.STORE 默认的应用商店 2.HTTP网络渠道|\n|3.退出                               |")
+        print("=======================================")
+        DOWN_INPUT = input("MayDOS/download>>>")
+        if DOWN_INPUT == '1':
+            print("======================================================")
+            print("已选择下载源<STORE>,可下载已上传的应用,输入EXIT以退出|")
+            print("======================================================")
+            while True:
+                D_I_0 = input("MayDOS/Download/STORE>>>")
+                if D_I_0 == 'EXIT' or D_I_0 == 'exit' or D_I_0 == 'Exit':
+                    break
+
+        elif DOWN_INPUT == '2': 
+            print("=========================================================")
+            print("已选择下载源<HTTP>,可下载任何网络上的应用,输入EXIT以退出|")
+            print("=========================================================")
+            while True:
+                D_I_0 = input("MayDOS/Download/STORE>>>")
+                if D_I_0.lower() == 'exit':
+                    break
+                else:
+                    wget.download(f'{D_I_0}',"important/download/")
+
+        elif DOWN_INPUT == '3' or DOWN_INPUT == '': 
+            pass
+        else:
+            print("未知操作,已自动退出......")
+            print("===================================================")
 
     elif cmd == "":
         pass
 
-    elif cmd == 'usebook':
-        print(Fore.GREEN + '键入"calc"以打开计算器')
-        print(Fore.GREEN + '键入"close"以关闭PyDOS0.1')
-        print(Fore.GREEN + '键入"notepad"以打开记事本程序')
-        print(Fore.GREEN + '键入"explorer"以打开资源管理器')
-        print(Fore.GREEN + '键入"cls"以清屏')
-        print(Fore.GREEN + '键入"sysver"以查看系统版本')
-        print(Fore.GREEN + '键入"sof <程序名称>"以打开第三方应用程序')
+    elif cmd[0:6].lower() == 'search':
+        search_cmd = r'START https://cn.bing.com/search?q='+cmd[7:]+'&cvid=aff84598b5bf4a62acc130c33917054f&aqs=edge..69i57j0l3j69i59l2j69i60l3.1183j0j4&FORM=ANAB01&PC=U531'
+        try:
+            try:
+                os.popen(search_cmd)
+            except Exception as e:
+                os.popen(search_cmd)
+                print(" ")
+            else:
+                print(" ")
+        except Exception as e:
+            os.popen(search_cmd)
+            print(" ")
 
-    elif cmd == 'close':
+    elif cmd.lower() == 'usebook'  or cmd.lower() == 'help':
+        print(f'{Font.GREEN}SEARCH           互联网搜索')
+        print(f'{Font.GREEN}USEBOOK          获取帮助')
+        print(f'{Font.GREEN}HELP             获取帮助')
+        print(f'{Font.GREEN}MENU             查询程序来源')
+        print(f'{Font.GREEN}CALC             打开计算器')
+        print(f'{Font.GREEN}CLOSE            退出MayDOS')
+        print(f'{Font.GREEN}NOTEPAD          打开记事本程序')
+        print(f'{Font.GREEN}EXPLORER         打开资源管理器')
+        print(f'{Font.GREEN}CLS              清屏')
+        print(f'{Font.GREEN}SYSVER           查看系统版本')
+        print(f'{Font.GREEN}DOWN             下载应用程序')
+        print(f'{Font.GREEN}SOF <Name>       打开任何应用程序')
+        print(f'{Font.BLUE}=====================================')
+        print(f'{Font.YELLOW}SHUT             关机(不是闹着玩的！){Font.WHITE}')
+
+    elif cmd.lower() == 'close':
         quit()
 
-    elif cmd == 'notepad':
-        try:
-            cls()
-            os.system('python important/Applications/Notepad.py')
-        except:
-            print(Fore.RED + f'找不到{cmd}应用程序')
+    elif cmd.lower() == 'menu':
+        pass
 
-    elif cmd == 'explorer':
-        try:
-            cls()
-            os.system('python important/Applications/Explorer.py')
-        except:
-            print(Fore.RED + f'找不到{cmd}应用程序')
+    elif cmd.lower() == 'shut':
+        if sys.platform == 'win32':
+            os.system('shutdown -p')
+        elif sys.platform == 'linux':
+            os.system('shutdown –h now')
+        elif sys.platform == 'darwin':
+            os.system('sudo shutdown -h now')
+        else:
+            os.system('shutdown -p')
 
-    elif cmd == 'cls':
-        cls()
+    elif cmd.lower() == 'notepad':
+        try:
+            ENCO_FILE = 'w'
+            print('\n选择编辑模式 -->')
+            print('1.正常写入')
+            print('2.系统权限写入')
+            print('3.特殊文件写入')
+            print(f'{Font.YELLOW}================')
+            print(f'{Font.BEIGE}exit        退出')
+            print(f'{Font.BEIGE}path    文件路径{Font.WHITE}\n')
+            print(f'{Font.BEIGE}enco    读取方式{Font.WHITE}\n')
+            Notepad = input('MayDOS/Apply/NOTEPAD>>>')
+            if Notepad == 'exit':
+                print('\n')
+            elif Notepad == '1':
+                print(f'模式:正常写入\n')
+                CFILE = input("MayDOS/Apply/NOTEPAD/CreatFile>>>")
+                with open(CFILE,ENCO_FILE,encoding='utf-8') as f:
+                    f.write(CFILE)
+                    f.close()
+            #SysPerAPI().cls()
+            #os.system('python important/Applications/Notepad/Notepad.py')
+        except Exception as e:
+            print(f'{Font.RED}MayDOS/Root/ERROR>>>{e}{Font.WHITE}')
+
+    elif cmd.lower() == 'explorer':
+        try:
+            SysPerAPI().cls()
+            os.system('python important/Applications/Explorer/Explorer.py')
+        except:
+            print(f'{Font.RED}找不到{cmd}应用程序{Font.WHITE}')
+
+    elif cmd.lower() == 'cls':
+        SysPerAPI().cls()
     elif cmd == username:
         JiTanLaiLuo = 0
         List_RAN = ['MayDOS有摸鱼部门和搞事部门！','0.4.1是0.4.2之前最多BUG的版本',
@@ -269,10 +476,8 @@ while True:
                 tkinter.messagebox.showingo(title="你知道吗?",message=f'{random.choice(List_RAN)}')
                 JiTanLaiLuo += 1
 
-    elif cmd =='sysver':
+    elif cmd.lower() =='sysver':
         print(f'系统版本：MayDOS {Ver}')
         print('\n开发：MayDOS开发团队 版权所有2023(C)')
     else:
-        print(Fore.YELLOW + "未定义的指令",cmd,Fore.YELLOW + "，请输入'usebook'以查看使用手册和帮助")
-
-root.mainloop()
+        print(f"{Font.YELLOW}未定义的指令，请输入'usebook'以查看使用手册和帮助{Font.WHITE}")
